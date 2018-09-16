3a71fe9b77bc48530be5c8ba115a8d4e0a4efc70
Dmitry Khominich <d.khominich@corp.mail.ru>
2018-09-16 19:20:38 +0300

    [+] docs: package doc

---
+++ doc.go
@@ -0,0 +1,6 @@
+/*
+Package leaves is pure Go implemetation of prediction part for GBRT (Gradient
+Boosting Regression Trees) models from popular frameworks. See leaves_test.go
+for usage examples
+*/
+package leaves
