fa2d0081adf122519826d1aee3d8ee3abeab4845
Dmitry Khominich <khdmitryi@gmail.com>
2018-09-16 11:49:20 +0300

    [+] agaricus test case on xgboost ensemble

--- leaves_test.go
+++ leaves_test.go
@@ -352,3 +352,54 @@ func BenchmarkHiggs(b *testing.B) {
 		model.PredictCSR(mat.RowHeaders, mat.ColIndexes, mat.Values, predictions, 0)
 	}
 }
+
+func TestAgaricus(t *testing.T) {
+	// loading test data
+	path := filepath.Join("testdata", "agaricus_test.libsvm")
+	reader, err := os.Open(path)
+	if err != nil {
+		t.Skipf("Skipping due to absence of %s", path)
+	}
+	bufReader := bufio.NewReader(reader)
+	mat, err := CSRMatFromLibsvm(bufReader, 0, true)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// loading model
+	path = filepath.Join("testdata", "agaricus.model")
+	reader, err = os.Open(path)
+	if err != nil {
+		t.Fatal(err)
+	}
+	bufReader = bufio.NewReader(reader)
+
+	model, err := XGEnsembleFromReader(bufReader)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if model.NTrees() != 3 {
+		t.Fatalf("expected 3 trees (got %d)", model.NTrees())
+	}
+
+	// loading true predictions as DenseMat
+	path = filepath.Join("testdata", "agaricus_true_predictions.txt")
+	reader, err = os.Open(path)
+	if err != nil {
+		t.Skipf("Skipping due to absence of %s", path)
+	}
+	bufReader = bufio.NewReader(reader)
+	truePredictions, err := DenseMatFromCsv(bufReader, 0, false, ",", 0.0)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// do predictions
+	predictions := make([]float64, mat.Rows())
+	model.PredictCSR(mat.RowHeaders, mat.ColIndexes, mat.Values, predictions, 0)
+	SigmoidFloat64SliceInplace(predictions)
+	// compare results
+	if err := almostEqualFloat64Slices(truePredictions.Values, predictions, 1e-7); err != nil {
+		t.Fatalf("different predictions: %s", err.Error())
+	}
+}
