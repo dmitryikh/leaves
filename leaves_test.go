ac4fcb0f1479980934804506aeb87c9894f7af60
Dmitry Khominich <d.khominich@corp.mail.ru>
2018-09-18 18:34:02 +0300

    [+] tests&benchmarks on parallel predictions

--- leaves_test.go
+++ leaves_test.go
@@ -8,6 +8,13 @@ import (
 )
 
 func TestLGMSLTR(t *testing.T) {
+	InnerTestLGMSLTR(t, 1)
+	InnerTestLGMSLTR(t, 2)
+	InnerTestLGMSLTR(t, 3)
+	InnerTestLGMSLTR(t, 4)
+}
+
+func InnerTestLGMSLTR(t *testing.T, nThreads int) {
 	// loading test data
 	path := filepath.Join("testdata", "msltr_1000examples_test.libsvm")
 	reader, err := os.Open(path)
@@ -46,7 +53,7 @@ func TestLGMSLTR(t *testing.T) {
 
 	// do predictions
 	predictions := make([]float64, mat.Rows())
-	model.PredictCSR(mat.RowHeaders, mat.ColIndexes, mat.Values, predictions, 0)
+	model.PredictCSR(mat.RowHeaders, mat.ColIndexes, mat.Values, predictions, 0, nThreads)
 
 	// compare results
 	if err := almostEqualFloat64Slices(truePredictions.Values, predictions, 1e-5); err != nil {
@@ -55,6 +62,13 @@ func TestLGMSLTR(t *testing.T) {
 }
 
 func TestLGHiggs(t *testing.T) {
+	InnerTestLGHiggs(t, 1)
+	InnerTestLGHiggs(t, 2)
+	InnerTestLGHiggs(t, 3)
+	InnerTestLGHiggs(t, 4)
+}
+
+func InnerTestLGHiggs(t *testing.T, nThreads int) {
 	// loading test data
 	path := filepath.Join("testdata", "higgs_1000examples_test.libsvm")
 	reader, err := os.Open(path)
@@ -62,10 +76,11 @@ func TestLGHiggs(t *testing.T) {
 		t.Skipf("Skipping due to absence of %s", path)
 	}
 	bufReader := bufio.NewReader(reader)
-	mat, err := CSRMatFromLibsvm(bufReader, 0, true)
+	csrMat, err := CSRMatFromLibsvm(bufReader, 0, true)
 	if err != nil {
 		t.Fatal(err)
 	}
+	nRows := csrMat.Rows()
 
 	// loading model
 	path = filepath.Join("testdata", "lghiggs.model")
@@ -91,9 +106,8 @@ func TestLGHiggs(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	// do predictions
-	predictions := make([]float64, mat.Rows())
-	model.PredictCSR(mat.RowHeaders, mat.ColIndexes, mat.Values, predictions, 0)
+	predictions := make([]float64, nRows)
+	model.PredictCSR(csrMat.RowHeaders, csrMat.ColIndexes, csrMat.Values, predictions, 0, nThreads)
 
 	// compare results
 	if err := almostEqualFloat64Slices(truePredictions.Values, predictions, 1e-12); err != nil {
@@ -103,6 +117,20 @@ func TestLGHiggs(t *testing.T) {
 
 func TestXGHiggs(t *testing.T) {
 	t.Skip("have mismatch on 45 element")
+	// Dense matrix
+	InnerTestXGHiggs(t, 1, true)
+	InnerTestXGHiggs(t, 2, true)
+	InnerTestXGHiggs(t, 3, true)
+	InnerTestXGHiggs(t, 4, true)
+
+	// CSR matrix
+	InnerTestXGHiggs(t, 1, false)
+	InnerTestXGHiggs(t, 2, false)
+	InnerTestXGHiggs(t, 3, false)
+	InnerTestXGHiggs(t, 4, false)
+}
+
+func InnerTestXGHiggs(t *testing.T, nThreads int, dense bool) {
 	// loading test data
 	path := filepath.Join("testdata", "higgs_1000examples_test.libsvm")
 	reader, err := os.Open(path)
@@ -110,9 +138,21 @@ func TestXGHiggs(t *testing.T) {
 		t.Skipf("Skipping due to absence of %s", path)
 	}
 	bufReader := bufio.NewReader(reader)
-	mat, err := CSRMatFromLibsvm(bufReader, 0, true)
-	if err != nil {
-		t.Fatal(err)
+	var denseMat DenseMat
+	var csrMat CSRMat
+	var nRows uint32
+	if dense {
+		denseMat, err = DenseMatFromLibsvm(bufReader, 0, true)
+		if err != nil {
+			t.Fatal(err)
+		}
+		nRows = denseMat.Rows
+	} else {
+		csrMat, err = CSRMatFromLibsvm(bufReader, 0, true)
+		if err != nil {
+			t.Fatal(err)
+		}
+		nRows = csrMat.Rows()
 	}
 
 	// loading model
@@ -139,17 +179,27 @@ func TestXGHiggs(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	// do predictions
-	predictions := make([]float64, mat.Rows())
-	model.PredictCSR(mat.RowHeaders, mat.ColIndexes, mat.Values, predictions, 0)
-
+	predictions := make([]float64, nRows)
+	if dense {
+		model.PredictDense(denseMat.Values, denseMat.Rows, denseMat.Cols, predictions, 0, nThreads)
+	} else {
+		model.PredictCSR(csrMat.RowHeaders, csrMat.ColIndexes, csrMat.Values, predictions, 0, nThreads)
+	}
 	// compare results
 	if err := almostEqualFloat64Slices(truePredictions.Values, predictions, 1e-5); err != nil {
 		t.Fatalf("different predictions: %s", err.Error())
 	}
 }
 
-func BenchmarkLGMSLTR(b *testing.B) {
+func BenchmarkLGMSLTR_csr_1thread(b *testing.B) {
+	InnerBenchmarkLGMSLTR(b, 1)
+}
+
+func BenchmarkLGMSLTR_csr_4thread(b *testing.B) {
+	InnerBenchmarkLGMSLTR(b, 4)
+}
+
+func InnerBenchmarkLGMSLTR(b *testing.B, nThreads int) {
 	// loading test data
 	path := filepath.Join("testdata", "msltr_1000examples_test.libsvm")
 	reader, err := os.Open(path)
@@ -178,11 +228,27 @@ func BenchmarkLGMSLTR(b *testing.B) {
 	b.ResetTimer()
 	predictions := make([]float64, mat.Rows())
 	for i := 0; i < b.N; i++ {
-		model.PredictCSR(mat.RowHeaders, mat.ColIndexes, mat.Values, predictions, 0)
+		model.PredictCSR(mat.RowHeaders, mat.ColIndexes, mat.Values, predictions, 0, nThreads)
 	}
 }
 
-func BenchmarkLGHiggs(b *testing.B) {
+func BenchmarkLGHiggs_dense_1thread(b *testing.B) {
+	InnerBenchmarkLGHiggs(b, 1, true)
+}
+
+func BenchmarkLGHiggs_dense_4thread(b *testing.B) {
+	InnerBenchmarkLGHiggs(b, 4, true)
+}
+
+func BenchmarkLGHiggs_csr_1thread(b *testing.B) {
+	InnerBenchmarkLGHiggs(b, 1, false)
+}
+
+func BenchmarkLGHiggs_csr_4thread(b *testing.B) {
+	InnerBenchmarkLGHiggs(b, 4, false)
+}
+
+func InnerBenchmarkLGHiggs(b *testing.B, nThreads int, dense bool) {
 	// loading test data
 	path := filepath.Join("testdata", "higgs_1000examples_test.libsvm")
 	reader, err := os.Open(path)
@@ -190,9 +256,21 @@ func BenchmarkLGHiggs(b *testing.B) {
 		b.Skipf("Skipping due to absence of %s", path)
 	}
 	bufReader := bufio.NewReader(reader)
-	mat, err := CSRMatFromLibsvm(bufReader, 0, true)
-	if err != nil {
-		b.Fatal(err)
+	var denseMat DenseMat
+	var csrMat CSRMat
+	var nRows uint32
+	if dense {
+		denseMat, err = DenseMatFromLibsvm(bufReader, 0, true)
+		if err != nil {
+			b.Fatal(err)
+		}
+		nRows = denseMat.Rows
+	} else {
+		csrMat, err = CSRMatFromLibsvm(bufReader, 0, true)
+		if err != nil {
+			b.Fatal(err)
+		}
+		nRows = csrMat.Rows()
 	}
 
 	// loading model
@@ -209,13 +287,26 @@ func BenchmarkLGHiggs(b *testing.B) {
 
 	// do benchmark
 	b.ResetTimer()
-	predictions := make([]float64, mat.Rows())
-	for i := 0; i < b.N; i++ {
-		model.PredictCSR(mat.RowHeaders, mat.ColIndexes, mat.Values, predictions, 0)
+	predictions := make([]float64, nRows)
+	if dense {
+		for i := 0; i < b.N; i++ {
+			model.PredictDense(denseMat.Values, denseMat.Rows, denseMat.Cols, predictions, 0, nThreads)
+		}
+	} else {
+		for i := 0; i < b.N; i++ {
+			model.PredictCSR(csrMat.RowHeaders, csrMat.ColIndexes, csrMat.Values, predictions, 0, nThreads)
+		}
 	}
 }
 
-func TestXGAgaricus(t *testing.T) {
+func TestXGAgaricus_1thread(t *testing.T) {
+	InnerTestXGAgaricus(t, 1)
+	InnerTestXGAgaricus(t, 2)
+	InnerTestXGAgaricus(t, 3)
+	InnerTestXGAgaricus(t, 4)
+}
+
+func InnerTestXGAgaricus(t *testing.T, nThreads int) {
 	// loading test data
 	path := filepath.Join("testdata", "agaricus_test.libsvm")
 	reader, err := os.Open(path)
@@ -258,7 +349,7 @@ func TestXGAgaricus(t *testing.T) {
 
 	// do predictions
 	predictions := make([]float64, mat.Rows())
-	model.PredictCSR(mat.RowHeaders, mat.ColIndexes, mat.Values, predictions, 0)
+	model.PredictCSR(mat.RowHeaders, mat.ColIndexes, mat.Values, predictions, 0, nThreads)
 	SigmoidFloat64SliceInplace(predictions)
 	// compare results
 	if err := almostEqualFloat64Slices(truePredictions.Values, predictions, 1e-7); err != nil {
@@ -266,7 +357,23 @@ func TestXGAgaricus(t *testing.T) {
 	}
 }
 
-func BenchmarkXGHiggs(b *testing.B) {
+func BenchmarkXGHiggs_dense_1thread(b *testing.B) {
+	InnerBenchmarkXGHiggs(b, 1, true)
+}
+
+func BenchmarkXGHiggs_dense_4thread(b *testing.B) {
+	InnerBenchmarkXGHiggs(b, 4, true)
+}
+
+func BenchmarkXGHiggs_csr_1thread(b *testing.B) {
+	InnerBenchmarkXGHiggs(b, 1, false)
+}
+
+func BenchmarkXGHiggs_csr_4thread(b *testing.B) {
+	InnerBenchmarkXGHiggs(b, 4, false)
+}
+
+func InnerBenchmarkXGHiggs(b *testing.B, nThreads int, dense bool) {
 	// loading test data
 	path := filepath.Join("testdata", "higgs_1000examples_test.libsvm")
 	reader, err := os.Open(path)
@@ -274,9 +381,21 @@ func BenchmarkXGHiggs(b *testing.B) {
 		b.Skipf("Skipping due to absence of %s", path)
 	}
 	bufReader := bufio.NewReader(reader)
-	mat, err := CSRMatFromLibsvm(bufReader, 0, true)
-	if err != nil {
-		b.Fatal(err)
+	var denseMat DenseMat
+	var csrMat CSRMat
+	var nRows uint32
+	if dense {
+		denseMat, err = DenseMatFromLibsvm(bufReader, 0, true)
+		if err != nil {
+			b.Fatal(err)
+		}
+		nRows = denseMat.Rows
+	} else {
+		csrMat, err = CSRMatFromLibsvm(bufReader, 0, true)
+		if err != nil {
+			b.Fatal(err)
+		}
+		nRows = csrMat.Rows()
 	}
 
 	// loading model
@@ -293,9 +412,14 @@ func BenchmarkXGHiggs(b *testing.B) {
 
 	// do benchmark
 	b.ResetTimer()
-	predictions := make([]float64, mat.Rows())
-	for i := 0; i < b.N; i++ {
-		model.PredictCSR(mat.RowHeaders, mat.ColIndexes, mat.Values, predictions, 0)
+	predictions := make([]float64, nRows)
+	if dense {
+		for i := 0; i < b.N; i++ {
+			model.PredictDense(denseMat.Values, denseMat.Rows, denseMat.Cols, predictions, 0, nThreads)
+		}
+	} else {
+		for i := 0; i < b.N; i++ {
+			model.PredictCSR(csrMat.RowHeaders, csrMat.ColIndexes, csrMat.Values, predictions, 0, nThreads)
+		}
 	}
-	SigmoidFloat64SliceInplace(predictions)
 }
